0492de1d5b2a1a0f61b34d3c277b74fa7ec69c1b
Nikolas Matias <nikolas.matias500@gmail.com>
2024-11-25 09:13:06 +0000

    testando o codespace

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Adocica meu amor! Na Tabnews!</h1>
+    return <h1>Adocica meu amor! Na Tabnews! Agora vou testar na codespace também baby!</h1>
 }
 
 export default Home;
